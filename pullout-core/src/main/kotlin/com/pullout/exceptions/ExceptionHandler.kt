6f3e0d0c9bec878c9cd4cee7940ee9178254283f
Seonggil-Jeong <sgjeong1108@gmail.com>
2023-01-29 00:18:07 +0900

    feat [#7] : add Exception handler

--- pullout-core/src/main/kotlin/com/pullout/exceptions/ExceptionHandler.kt
+++ pullout-core/src/main/kotlin/com/pullout/exceptions/ExceptionHandler.kt
@@ -1,36 +1,71 @@
 package com.pullout.exceptions
 
+import com.fasterxml.jackson.databind.exc.ValueInstantiationException
+import com.pullout.app.utils.DateTimeUtil
+import mu.KLogging
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.MethodArgumentNotValidException
-import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.time.LocalDateTime
+import javax.validation.ConstraintViolationException
+
 
 @Component
-@RestControllerAdvice
-class ExceptionHandler : ResponseEntityExceptionHandler() {
+@ControllerAdvice
+class ExceptionHandler {
 
-    override fun handleMethodArgumentNotValid(
-        ex: MethodArgumentNotValidException,
-        headers: HttpHeaders,
-        status: HttpStatus,
-        request: WebRequest
-    ): ResponseEntity<Any> {
-        val errors = ex.bindingResult.allErrors
-            .map { error -> error.defaultMessage!! }
-            .sorted()
+    companion object : KLogging()
 
-        logger.error("Validation Exception occur : $ex")
+    /**
+     * All Exception Handler
+     */
+    @ExceptionHandler(Exception::class)
+    fun handleException(exception: Exception): ResponseEntity<ErrorResponse?>? {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+            ErrorResponse(
+                code = "INTERNAL_SERVER_ERROR",
+                message = exception.message.toString()
+            )
+        )
+    }
 
+    /**
+     * Validate Request Param
+     */
+    @ExceptionHandler
+    fun handleConstraintViolationException(e: ConstraintViolationException): ResponseEntity<ErrorResponse> {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse("VALIDATION_FAILED", errors[0]))
+            .body(
+                ErrorResponse(
+                    code = HttpStatus.BAD_REQUEST.toString(),
+                    message = e.message.toString()
+                )
+            )
+    }
+
+    /**
+     * Validate Response Body
+     */
+    @ExceptionHandler
+    fun handleInvalidRequestBodyException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        logger.error { "occur Validation Exception : ${e.message}" }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ErrorResponse(
+                code = HttpStatus.BAD_REQUEST.toString(),
+                message = e.bindingResult.allErrors.map { it.defaultMessage }[0].toString()
+            )
+        )
+
     }
 
     data class ErrorResponse(
         val code: String,
-        val message: String
+        val message: String,
+        val occurExceptionTime: LocalDateTime = DateTimeUtil.kstDateTimeNow()
     )
 }
