efd0631e95e5bbf5bc86d316b0c11152d209f248
ohju96 <ohju96@naver.com>
2023-01-28 17:42:08 +0900

    feat [#2] : Init Security and Security configuration setting and healthcheck API

---
+++ pullout-core/src/main/kotlin/com/pullout/app/config/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package com.pullout.app.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf().disable()
+
+        http.authorizeHttpRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/healthcheck").permitAll()
+            .antMatchers("/mypage").hasRole("USER")
+            .anyRequest().authenticated()
+
+        return http.build()
+    }
+}
